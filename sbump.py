1bc75deb9da0c2da7ab2864e72152790f9827493
Jinna Kiisuo <jinnak@nocturnal.fi>
2024-09-27 18:52:53 +0300

    feat: implement semver bumps

--- sbump.py
+++ sbump.py
@@ -1,24 +1,63 @@
 import click
-import tomllib
+import tomlkit
+from tomlkit.toml_file import TOMLFile
 import semver
 
-
-def get_from_pyproject():
-    with open("pyproject.toml", "rb") as f:
-        pyproject = tomllib.load(f)
-    return semver.Version.parse(pyproject['project']['version'])
+PYPROJECT: TOMLFile = TOMLFile("pyproject.toml")
 
 
-@click.command()
+def get_from_pyproject() -> semver.Version:
+    pyproject: tomlkit.TOMLDocument = PYPROJECT.read()
+    version: str = str(pyproject["project"]["version"])  # pyright: ignore
+    return semver.Version.parse(version)
+
+
+def update_pyproject(new_version: semver.Version):
+    pyproject: tomlkit.TOMLDocument = PYPROJECT.read()
+    pyproject["project"]["version"] = str(new_version)  # pyright: ignore
+    PYPROJECT.write(pyproject)
+
+
+@click.group(invoke_without_command=True)
+@click.pass_context
+@click.option(
+    "--dry_run/--no_dry_run",
+    default=False,
+    help="Don't write changes to pyproject.toml",
+)
+def cli(ctx):
+    if not ctx.invoked_subcommand:
+        display()
+
+
+@cli.command()  # pyright: ignore
 def display():
     version = get_from_pyproject()
     click.echo(version)
 
 
-def run():
-    # By default we display the current version as read from pyproject.toml
-    display()
+@cli.command()  # pyright: ignore
+def major(dry_run: bool):
+    version = get_from_pyproject()
+    new_version = version.bump_major()
+    click.echo(new_version)
+    if not dry_run:
+        update_pyproject(new_version)
 
 
-if __name__ == "__main__":
-    run()
+@cli.command()  # pyright: ignore
+def minor(dry_run: bool):
+    version = get_from_pyproject()
+    new_version = version.bump_minor()
+    click.echo(new_version)
+    if not dry_run:
+        update_pyproject(new_version)
+
+
+@cli.command()  # pyright: ignore
+def patch(dry_run: bool):
+    version = get_from_pyproject()
+    new_version = version.bump_patch()
+    click.echo(new_version)
+    if not dry_run:
+        update_pyproject(new_version)
